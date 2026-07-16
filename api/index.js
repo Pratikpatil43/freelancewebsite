2264cf80b659970fdf9753ab6bda6c75e740e981
Pratikpatil43 <patilpratik4388@gmail.com>
2026-07-16 22:36:06 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-import app from '../server/server.js';
+let appPromise;
 
-export default app;
+module.exports = async (req, res) => {
+  appPromise ||= import('../server/server.js').then(({ default: app }) => app);
+  const app = await appPromise;
+  return app(req, res);
+};
