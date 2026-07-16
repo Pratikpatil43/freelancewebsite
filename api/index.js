7f97a4e0a985b8cdc76318317b6cb24a6293a27e
Pratikpatil43 <patilpratik4388@gmail.com>
2026-07-16 22:42:06 +0530

    use native ESM Vercel function

--- api/index.js
+++
@@ -1,7 +0,0 @@
-let appPromise;
-
-module.exports = async (req, res) => {
-  appPromise ||= import('../server/server.js').then(({ default: app }) => app);
-  const app = await appPromise;
-  return app(req, res);
-};
